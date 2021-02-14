bd40d62b4621edd35ce787b769130e9a2aefaa2e
Theemeraldemera <jadielocollazo89@gmail.com>
2021-02-14 16:55:44 +0000

    e

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import pbfplayer as p
+import Engine.pbfplayer as p
 
 #frame loop !1!
 while True:
-    p.screen()
-    print(p.time.ctime(p.time.time()))
-    #if input requested
-    if p.taking_inp == True:
-        p.in_channel=input("> ")
-    p.sleep(0.5)
-    p.clear()
-    
+	p.screen()
+	print(p.time.ctime(p.time.time()))
+	print(p.taking_inp)
+	#if input requested
+	if p.taking_inp == True:
+		p.in_channel = input("> ")
+	p.sleep(0.5)
+	p.clear()
